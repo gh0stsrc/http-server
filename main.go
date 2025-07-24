89f8422b7a0a7e850f3266f7a22d3034975e9d66
gh0stsrc <malkowskimathew@gmail.com>
2025-07-23 20:50:39 -0400

    chore: channel version

--- main.go
+++ main.go
@@ -7,13 +7,45 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	buffer := bytes.NewBuffer(make([]byte, 0, 8))
+	var currentLine string
+
+	go func() {
+		defer close(ch)
+		defer f.Close()
+		for {
+			_, err := io.CopyN(buffer, f, 8)
+			currentLine = currentLine + buffer.String()
+			parts := strings.Split(currentLine, "\n")
+			buffer.Reset()
+			if len(parts) > 1 {
+				msg := ""
+				for i := range len(parts) - 1 {
+					msg = msg + parts[i]
+				}
+				ch <- msg
+				currentLine = parts[len(parts)-1]
+			}
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				slog.Error("reading from file to buffer", "error", err)
+			}
+		}
+	}()
+
+	return ch
+}
 
 func main() {
 
 	workingDir, err := os.Getwd()
-	s := make([]byte, 0, 8)
-	buffer := bytes.NewBuffer(s)
 	if err != nil {
 		slog.Error("get working dir", "error", err)
 		os.Exit(1)
@@ -25,15 +57,15 @@ func main() {
 	}
 	defer f.Close()
 
+	ch := getLinesChannel(f)
+
 	for {
-		_, err := io.CopyN(buffer, f, 8)
-		fmt.Printf("read: %s\n", buffer.String())
-		buffer.Reset()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			slog.Error("reading from file to buffer", "error", err)
+		line, ok := <-ch
+		if !ok {
+			ch = nil
+			break
 		}
+		fmt.Printf("read: %s\n", line)
 	}
+
 }
